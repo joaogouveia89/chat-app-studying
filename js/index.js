963bd705ba47f2de626f2475c3bc25d8911d174b
Joao Gouveia <jgouveia@avenuecode.com>
2016-07-04 17:44:29 -0300

    partial done

--- js/index.js
+++ js/index.js
@@ -3,5 +3,15 @@ $(document).ready(function(){
   var nickname;
   socket.on('connect', function(data){
     $('#identifying-modal').openModal();
+    $("#join-request").click(function(){
+      var nickname = $("#nickname").val().trim();
+      if(nickname == undefined || nickname === "" || nickname == null){
+        Materialize.toast("Nickname cannot be empty!", 5000);
+        socket = io.connect('http://localhost:8080', {
+          'reconnect': true,
+          'reconnection delay': 500
+        });
+      }
+    });
   });
 });
